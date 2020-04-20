e37c9ccdc3012266824051ec8ebe73b0a1302d1e
acasado <acasadoquijada@gmail.com>
2020-04-20 11:39:28 +0200

    Added more jokes to funnyJokes closes #1

--- funnyJoke/src/main/java/com/example/jokelibrary/FunnyJokes.java
+++ funnyJoke/src/main/java/com/example/jokelibrary/FunnyJokes.java
@@ -5,13 +5,22 @@ import java.util.Random;
 
 /**
  * Java class that supplies with jokes
+ *
+ * Jokes from here: https://short-funny.com/one-liners.php
  */
 public class FunnyJokes {
 
     private final static String[] myJokes = {
             "I dreamed I was forced to eat a giant marshmallow. When I woke up, my pillow was gone.",
             "Have you ever tried eating a clock? It's really time-consuming, especially if you go for seconds.",
-            "Where do fish sleep? In the riverbed."
+            "Where do fish sleep? In the riverbed.",
+            "My dog is an awesome fashion adviser. Every time I ask him what I look like in my clothes, he says WOW!",
+            "Mom! I’m a 3d printer! - Oh come on, Tommy, close the door when you poop.",
+            "Why do bees hum? They don't remember the text!",
+            "Why are ghost such bad liars? Because they are easy to see through.",
+            "I called the hospital but the line was dead.",
+            "If I got 50 cents for every failed math exam, I’d have 6.30 dollars now.",
+            " I heard women love a man in uniform. Can’t wait to start working at McDonalds."
             };
 
 
